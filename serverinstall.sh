4e244678a47f1248668fcd971accd3769b580fce
whynot <sumsung3580@gmail.com>
2024-11-01 16:05:51 +0400

    Update serverinstall.sh

--- serverinstall.sh
+++ serverinstall.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #============================================================================================================================
-FILE="/etc/apt/sources.list" #--переменная сорц листа
+srcl="/etc/apt/sources.list" #--переменная сорц листа
 sysctlc="/etc/sysctl.conf"
 applications=(apt-transport-https ca-certificates fail2ban mcedit) #------ Тут пишем аппсы, БЕЗ запятых, ТОЛЬКО с пробелами
 #============================================================================================================================
 printf "\033[93m Изменение репозиториев... \033[0m"
 #- чистим файл
-echo -e "" > $FILE
+echo -e "" > $srcl
 # Добавляем новые строки в файл
-echo -e "deb http://deb.debian.org/debian bookworm main non-free-firmware" >> $FILE
-echo -e "deb-src http://deb.debian.org/debian bookworm main non-free-firmware" >> $FILE
+echo -e "deb http://deb.debian.org/debian bookworm main non-free-firmware" >> $srcl
+echo -e "deb-src http://deb.debian.org/debian bookworm main non-free-firmware" >> $srcl
 #
-echo -e "deb http://security.debian.org/debian-security bookworm-security main non-free-firmware" >> $FILE
-echo -e "deb-src http://security.debian.org/debian-security bookworm-security main non-free-firmware" >> $FILE
+echo -e "deb http://security.debian.org/debian-security bookworm-security main non-free-firmware" >> $srcl
+echo -e "deb-src http://security.debian.org/debian-security bookworm-security main non-free-firmware" >> $srcl
 
-echo -e "deb http://deb.debian.org/debian bookworm-updates main non-free-firmware" >> $FILE
-echo -e "deb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware" >> $FILE
+echo -e "deb http://deb.debian.org/debian bookworm-updates main non-free-firmware" >> $srcl
+echo -e "deb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware" >> $srcl
 #
 #============================================================================================================================
 printf "\033[93m Запускается обновление системы... \033[0m"

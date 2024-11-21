34a1c8f99e2e575a64b34772c2a1b045a4d56099
whynot <sumsung3580@gmail.com>
2024-11-21 13:05:43 +0400

    Update testfile.sh

--- testfile.sh
+++ testfile.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-#============================================================================================================================
-srcl="/etc/apt/sources.list" #--переменная сорц листа
-sysctlc="/etc/sysctl.conf"
-applications=(apt-transport-https ca-certificates fail2ban mcedit curl) #------ Тут пишем аппсы, БЕЗ запятых, ТОЛЬКО с пробелами
-#============================================================================================================================
-printf "\033[93m Изменение репозиториев... \033[0m"
-#- чистим файл
-echo -e "" > $srcl
-# Добавляем новые строки в файл
-echo -e "deb http://deb.debian.org/debian bookworm main non-free-firmware" >> $srcl
-echo -e "deb-src http://deb.debian.org/debian bookworm main non-free-firmware" >> $srcl
-#
-echo -e "deb http://security.debian.org/debian-security bookworm-security main non-free-firmware" >> $srcl
-echo -e "deb-src http://security.debian.org/debian-security bookworm-security main non-free-firmware" >> $srcl
-
-echo -e "deb http://deb.debian.org/debian bookworm-updates main non-free-firmware" >> $srcl
-echo -e "deb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware" >> $srcl
-#
-#============================================================================================================================
-
-#
-printf "\033[93m Готово. \033[0m"
-read -p "Задачи завершены. После нажатия ENTER сервер ребутнется"
+read -p  Привет c: Нажми Энтер"
+reboot

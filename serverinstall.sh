d979e4095cb0b091047970744ed7383b11e24771
whynot <sumsung3580@gmail.com>
2024-11-21 09:25:20 +0400

    Update serverinstall.sh

--- serverinstall.sh
+++ serverinstall.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
+# RU ---------------------
 # А че делаем?
 # 1 - вносим изменения в сорц.лист, поскольку авторы дебиана за каким-то решили, что делать этот файл пустым - идея хорошая. Желаем им добра и меняем на нормальные
 # 2 - обновляемся
 # 3 - ставим аппсы: apt-transport-https, ca-certificates, fail2ban, mcedit, curl
-#  - вносим очень важный твик ядра, который --ЗНАЧИТЕЛЬНО-- увеличит производительность сетевой составляющей сервера, это особенно заметно,если подключений будет много
+# - поскольку у нас подразумевается лень ИЛИ обычный юзер - руками apt upgrade никто не делает - автоматизируем сервисами, нативно, без софта
+#  - вносим очень важный твик ядра, который --ЗНАЧИТЕЛЬНО-- увеличит производительность сетевой составляющей сервера, это особенно заметно, если подключений будет много
 #  - чистим старые пакеты
 #  - ставим 3x-ui и отдаем управление этому скрипту
 #  - ребутаемся и все
+# ENG ---------------------
+
 #============================================================================================================================
 srcl="/etc/apt/sources.list" #--переменная сорц листа
 sysctlc="/etc/sysctl.conf" #-- переменная сисцтл, куда внесем изменения для твика ядра
-appinst=(apt-transport-https ca-certificates fail2ban mcedit curl) #-- переменная цикла -- Тут пишем аппсы, БЕЗ запятых, ТОЛЬКО с пробелами
-autostscr="/usr/local/bin/autostart.sh"
-autoservc="/etc/systemd/system/auto_update.service"
-#appremove=(vim cron)
-#echo -e "" >> $var
+appinst=(fail2ban mcedit curl) #-- переменная цикла -- Тут пишем аппсы, БЕЗ запятых, ТОЛЬКО с пробелами
+autostscr="/usr/local/bin/autostart.sh" #--
+autoservc="/etc/systemd/system/AutoUpdate.service" #--
+autotimer="/etc/systemd/system/AutoUpdate.timer" #--
+#appremove=(vim cron) #-- не люблю вим, фу
+#echo -e "" >> $var #-- кртл + с
 #============================================================================================================================
 printf "\033[93m Изменение репозиториев... \033[0m"
 #- чистим файл
 echo -e "" > $srcl
+# Добавляем новые строки в файл БЕЗ HTTP-S- - это нужно как временное решение для установки сертов, если их нет, а их скорее всего и нет :с
+echo -e "\n# Основной репозиторий" >> $srcl
+echo "deb http://deb.debian.org/debian bookworm main non-free-firmware" >> $srcl
+apt update && apt install ca-certificates -y && apt install apt-transport-https -y
 # Добавляем новые строки в файл
-echo -e "deb http://deb.debian.org/debian bookworm main non-free-firmware" >> $srcl
-echo -e "deb-src http://deb.debian.org/debian bookworm main non-free-firmware" >> $srcl
+echo -e "deb https://deb.debian.org/debian bookworm main non-free-firmware" >> $srcl
+echo -e "deb-src https://deb.debian.org/debian bookworm main non-free-firmware" >> $srcl
 #
-echo -e "deb http://security.debian.org/debian-security bookworm-security main non-free-firmware" >> $srcl
-echo -e "deb-src http://security.debian.org/debian-security bookworm-security main non-free-firmware" >> $srcl
+echo -e "deb https://security.debian.org/debian-security bookworm-security main non-free-firmware" >> $srcl
+echo -e "deb-src https://security.debian.org/debian-security bookworm-security main non-free-firmware" >> $srcl
 #
-echo -e "deb http://deb.debian.org/debian bookworm-updates main non-free-firmware" >> $srcl
-echo -e "deb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware" >> $srcl
+echo -e "deb https://deb.debian.org/debian bookworm-updates main non-free-firmware" >> $srcl
+echo -e "deb-src https://deb.debian.org/debian bookworm-updates main non-free-firmware" >> $srcl
 #
 #============================================================================================================================
 printf "\033[93m Запускается обновление системы... \033[0m"
@@ -45,27 +54,36 @@ done
 echo "export EDITOR=/usr/bin/vim" >> ~/.bashrc
 export EDITOR=mcedit
 #============================================================================================================================
+#======================== ----------------------Автоматизация обновлений
+#============================================================================================================================
 #-- создаем скрипт, который послужит точкой выполнения автоапдейта
-#touch $autostscr
-#echo -e '#!/bin/bash' >> $autostscr
-#echo -e "apt update && apt upgrade -y" >> $autostscr
+touch $autostscr
+echo -e '#!/bin/bash' >> $autostscr
+echo -e "apt update && apt upgrade -y" >> $autostscr
 #--
-#chmod +x $autostscr
+chmod +x $autostscr
 #============================================================================================================================
 #-- Создаем службу, которая будет выполнять скрипт выше
-#touch $autoservc
-#echo -e "" >> $autoservc
-#[Unit]
-#Description="AutoUpdate Service"
-
-#[Service]
-#User=root
-#ExecStart=/usr/local/bin/autostart.sh
-
-#[Install]
-#WantedBy=multi-user.target
+touch $autoservc
+echo -e "[Unit]" >> $autoservc
+echo -e "Description="AutoUpdate Service"" >> $autoservc
+echo -e "[Service]" >> $autoservc
+echo -e "User=root" >> $autoservc
+echo -e "ExecStart=/usr/local/bin/autostart.sh" >> $autoservc
+echo -e "[Install]" >> $autoservc
+echo -e "WantedBy=multi-user.target" >> $autoservc
 #============================================================================================================================
-
+#-- Создаем таймер, который будет запускать этот сервис раз в месяц, в 12 часов ночи
+touch $autotimer
+echo -e "[Timer]" >>
+echo -e "OnCaleendar=Weekly" >> $autotimer
+echo -e "Unit=AutoUpdate.service" >> $autotimer
+#----------------------------------------------------------------------------------------------------------------------------
+systemd-analyze verify /etc/systemd/system/AutoUpdate.timer
+systemctl start $autotimer
+systemctl start $autostscr
+#============================================================================================================================
+#============================================================================================================================
 #for app in "${appremove[@]}"
 #do
 #    sudo apt remove -y "$app"
@@ -85,12 +103,13 @@ apt autoremove -y #--- чистим старые пакеты автоудаля
 printf "\033[93m А теперь внимательно - сейчас управление будет передано 3x-ui. \033[0m"
 printf "\033[93m ОЧЕНЬ внимательно читаем, че там будет написано. \033[0m"
 printf "\033[93m Он спросит - настроить-ли панель управления? НЕТ - идеально, т.к. нам нужно скрыть факт нахождения впн на своем сервере \033[0m"
-printf "\033[93m Прочитал? \033[0m"
-read -p "Жмем Энтер, начнется установка и ждем, пока он задаст вопрос."
+printf "\033[93m Потом, там появится информация, которая НЕОБХОДИМА для подключения к твоему серверу \033[0m"
+read -p "Прочитал? Точно? Жми Энтер."
+read -p "С первого раза обычно не читают, а это важно. Вот теперь начинаем установку после нажатия enter."
 #============================================================================================================================
 #--- 3x UI
 bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
 #============================================================================================================================
 #
 printf "\033[93m Готово. \033[0m"
-read -p "Задачи завершены. ОБЯЗАТЕЛЬНО сделай перезагрузку после того, как сохранишь данные для подключения"
+read -p "Задачи завершены. ОБЯЗАТЕЛЬНО сделай перезагрузку ПОСЛЕ того, как сохранишь данные для подключения"

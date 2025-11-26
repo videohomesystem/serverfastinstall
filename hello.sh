62bd008d24182886c506e1bb133c34d690f3aa67
whynot <23554032+videohomesystem@users.noreply.github.com>
2025-11-26 13:34:01 +0400

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -5,19 +5,23 @@
 #   3 - enjoy!
 #
 #        Hello message
-#   execute > /etc/profile.d/hello.sh
-#
+#        execute > /etc/profile.d/hello.sh
+#        tested on Denian 13
 #"\033[93m\n yellow text -\e[1;37m white text \033[0m\n"
 #
 hostname=$(hostname) 
 dspace=$(df -h / | awk 'NR==2{print "📦 " $2 " total | 💾 " $3 " used | 🆓 " $4 " free | 📊 " $5}')
 uptime=$(uptime -p)
 localip=$(hostname --ip-address)
-#===================================
-printf "\n\033[93m ####---- Welcome to\e[1;37m> $hostname <\033[93m ----#### \033[0m\n"
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+ufws=$(ufw status verbose)
+#fail2=$(fail2ban-client status | grep 'banned')
+#echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+printf "\n\e[1;37m------------------------*|||*------------------------\033[0m\n"
+printf "\n\033[93m [[[[---- Welcome to\e[1;37m> $hostname <\033[93m ----]]]] \033[0m\n"
+echo " "
 printf "%b" "\033[93m Disk space: \e[1;37m $dspace \033[0m\n"
 printf "\033[93m Uptime: \e[1;37m $uptime \033[0m\n"
 printf "\033[93m IP: \e[1;37m $localip \033[0m\n"
-printf " \033[0m"
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+printf "\033[93m*-=-=-=-=-=-=-=-=-\e[1;37m> UFW STATUS <\033[93m-=-=-=-=-=-=-=-=-* \033[0m\n"
+printf "%b" "\033[93m ufw: \e[1;37m $ufws \033[0m\n"
+printf "\n\e[1;37m------------------------*._.*------------------------\033[0m\n"
